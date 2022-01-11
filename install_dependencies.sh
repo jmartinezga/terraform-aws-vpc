66937c85a235d28e25112c9ed456eb58a96eca57
Jordi Martinez <jmartinez.galvez@gmail.com>
2022-01-11 20:21:20 +0100

    Fix: Add terragrunt

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -1,19 +1,22 @@
 #!/bin/bash
 
 # terraform-docs
-sudo curl -Lo ./terraform-docs.tar.gz https://github.com/terraform-docs/terraform-docs/releases/download/v0.16.0/terraform-docs-v0.16.0-$(uname)-amd64.tar.gz
+echo "Install terraform-docs"
+sudo curl -Lo ./terraform-docs.tar.gz "$(curl -s https://api.github.com/repos/terraform-docs/terraform-docs/releases/latest | grep -o -E "https://.+?-linux-amd64.tar.gz")"
 sudo tar -xzf ./terraform-docs.tar.gz
 sudo chmod +x ./terraform-docs
-sudo mv ./terraform-docs /usr/local/terraform-docs
-sudo rm -Rf ./terraform-docs.tar.gz
+sudo mv ./terraform-docs /usr/local/bin
+sudo rm -Rf ./terraform-docs.tar.gz LICENSE README.md
 
 # terrascan
+echo "Install terrascan"
 sudo curl -Lo ./terrascan.tar.gz "$(curl -s https://api.github.com/repos/accurics/terrascan/releases/latest | grep -o -E "https://.+?_Linux_x86_64.tar.gz")"
 sudo tar -xzf ./terrascan.tar.gz
 sudo mv ./terrascan /usr/local/bin
 sudo rm -Rf terrascan.tar.gz
 
 # tfsec
+echo "Install tfsec"
 sudo curl -Lo ./tfsec "$(curl -s https://api.github.com/repos/aquasecurity/tfsec/releases/latest | grep -o -E "https://.+?-linux-amd64" | sort -u)"
 sudo chmod +x ./tfsec
 sudo mv ./tfsec /usr/local/bin
@@ -21,7 +24,7 @@ sudo mv ./tfsec /usr/local/bin
 #tflint
 #https://github.com/terraform-linters/tflint/blob/master/docs/user-guide/config.md
 #https://github.com/terraform-linters/tflint-ruleset-aws
-
+echo "Install tflint"
 sudo curl -Lo ./tflint.zip "$(curl -s https://api.github.com/repos/terraform-linters/tflint/releases/latest | grep -o -E "https://.+?_linux_amd64.zip" | sort -u)"
 sudo unzip ./tflint.zip
 sudo mv ./tflint /usr/local/bin
@@ -40,3 +43,9 @@ plugin "aws" {
 }
 EOF
 tflint --init
+
+#https://github.com/gruntwork-io/terragrunt
+echo "Install Terragrunt"
+sudo curl -Lo ./terragrunt "$(curl -s https://api.github.com/repos/gruntwork-io/terragrunt/releases/latest | grep -o -E "https://.+?_linux_amd64" | sort -u)"
+sudo chmod +x ./terragrunt
+sudo mv ./terragrunt /usr/local/bin
